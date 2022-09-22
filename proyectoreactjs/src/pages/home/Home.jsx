2f534157882c37522cdc0658ced7a4c4a138d79c
valentina1901 <vgaviria415@misena.edu.co>
2022-09-22 18:19:06 -0500

    proyectoreact

---
+++ proyectoreactjs/src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+
+import './Home.css';
+//import Header from '../components/header/Header.jsx';
+//import Nav from '../components/nav/Nav.jsx';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="App-header">
+        <p>
+         siiii
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
